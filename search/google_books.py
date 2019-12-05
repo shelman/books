d179fbdf9ebf359ca619fa8915e25f6530048767
Sam Helman <shelman@flatiron.com>
2019-12-04 21:49:50 -0500

    Clean up app.py

--- search/google_books.py
+++ search/google_books.py
@@ -1,11 +1,24 @@
 import json
 import requests
-
+def dedupe(results):
+    seen = set()
+    filtered = []
+    for result in results:
+        if (
+                result["volumeInfo"]["authors"][0],
+                result["volumeInfo"]["title"],
+        ) not in seen:
+            filtered.append(result)
+            seen.add(
+                (result["volumeInfo"]["authors"][0], result["volumeInfo"]["title"])
+            )
+    return filtered
 
 def search_books(query):
     r = requests.get(f"https://www.googleapis.com/books/v1/volumes?q={query}")
     response_content = json.loads(r.content)
-    return response_content["items"]
+    return dedupe(response_content["items"])
+
 
 
 if __name__ == "__main__":
--- webapp/app.py
+++ webapp/app.py
@@ -5,43 +5,31 @@ from flask import Flask, render_template, request
 from search.google_books import search_books
 
 
-def dedupe(results):
-    seen = set()
-    filtered = []
-    for result in results:
-        if (
-            result["volumeInfo"]["authors"][0],
-            result["volumeInfo"]["title"],
-        ) not in seen:
-            filtered.append(result)
-            seen.add(
-                (result["volumeInfo"]["authors"][0], result["volumeInfo"]["title"])
-            )
-    return filtered
+class App(Flask):
 
+    def __init__(self, name, folder, *args, **kwargs):
+        template_folder = os.path.join(folder, "pages")
+        super().__init__(name, template_folder=template_folder, *args, **kwargs)
+        self.folder = folder
 
-def attach_routes(app):
-    @app.route("/")
-    def index():
-        return render_template("index.html")
+    def attach_index(self):
+        @self.route("/")
+        def index():
+            return render_template("index.html")
 
-    @app.route("/search")
-    def search():
-        results = search_books(request.args.get("query"))
-        results = dedupe(results)
-        return render_template("search.html", results=results)
+    def attach_search(self):
+        @self.route("/search")
+        def search():
+            results = search_books(request.args.get("query"))
+            return render_template("search/search.html", results=results)
 
-
-def create_app():
-    app_directory = os.path.dirname(os.path.realpath(__file__))
-    template_folder = os.path.join(app_directory, "templates")
-
-    app = Flask("books", template_folder=template_folder)
-    attach_routes(app)
-    app.debug = True
-    return app
+    def configure(self):
+        self.debug = True
 
 
 if __name__ == "__main__":
-    app = create_app()
+    app = App("books", os.path.dirname(os.path.realpath(__file__)))
+    app.configure()
+    app.attach_index()
+    app.attach_search()
     app.run()
